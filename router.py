3da4738da3c4d328a4fd1122abfda9cf6b092d5f
Davi Feitosa <davifeitosabastos@gmail.com>
2026-08-13 11:59:55 -0400

    added show_items to menus list

--- router.py
+++ router.py
@@ -1,6 +1,11 @@
-from ui.menus import add_items_menu, exit_program
+from ui.menus import add_items_menu, exit_program, show_items_menu
+
+
+
+
 
 router = {
     1: add_items_menu,
+    2: show_items_menu,
     0: exit_program
 }
