f087780fe7d9a28947b251013aaea4f2bfedd060
Davi Feitosa <davifeitosabastos@gmail.com>
2026-08-13 13:10:39 -0400

    refactor: organized the add_items_menu

--- ui/menus.py
+++ ui/menus.py
@@ -1,4 +1,3 @@
-from turtle import width
 from rich.panel import Panel
 from rich.console import Console
 from rich import print
@@ -33,43 +32,61 @@ def main_menu() -> None:
                 width=35
     ))
 
-    
+
 def add_items_menu():
-    MEASURES = ['kg', 'g', 'lb', 'oz', 'm', 'cm', 'mm', 'in', 'ft', 'l', 'ml']
-    
     while True:
         clear()
         print(Panel(
-                "[#57e389]Would you like to add an item?\n"
-                "[#57e389]1 -> Yes\n" 
-                "[#57e389]2 -> No", 
-                style='#10d610',
-                title="★ Add items ★",
-                subtitle="Informations here",
-                width=40,
+            "[#57e389]Would you like to add something?\n"
+            "[#57e389]1 - to add\n"
+            "[#57e389]0 - to return"
+            ,style='#10d610',
+            title='★ Adding items Menu ★',
+            subtitle='Type here',
+            width=40
         ))
-        choice = get_options()
-        if choice == 1: 
+
+        option = get_options()
+
+        if option == 0:
             clear()
-            name = get_name()
-            clear()
-            amount = get_amout()
+            break
+            
+        if option == 1:               
             clear()
             print(Panel(
-                f"| kg | g | lb | oz | m | cm | mm | in | ft | l | ml |",
-                style='#10d610',
-                title="★ Kind of measures ★",
-                subtitle="Type here",
-                width=57,
+                "[#57e389]Here you can name the item[/] [bold](e.g: Milk, Snacks)\n"
+                "[#57e389]Obs. You can type lower either way"
+                ,style='#10d610',
+                title='★ Adding items Menu ★',
+                subtitle='Type the name here',
+                width=50
             ))
-            measure = get_measures(MEASURES)
-            add_items_to_list(name, amount, generate_id(load_list), measure, load_list, list_path)    
+            name = get_name()
+            if name is None:
+                continue
+            print(f"[#57e389]Name added {name}")
+            sleep(0.8)
+            clear()
+            
+            print(Panel(
+                "[#57e389]Now type the amount and its measure (e.g: 15 ml etc...)[/]\n"
+                "[#57e389]Obs. Only integer or float (e.g: 1, 1.5, 2.3, 4 etc..)\n\n"
+                "If you don't type the measure it's going to be 'unit'",
+                style='#10d610',
+                title='★ Adding items Menu ★',
+                subtitle='Type the amount/measure here',
+                width=60
+            ))
+            
+            amount = get_amout()
+            measure = get_measures()
+            print(f"[#57e389]The amount of {name} is {amount} {measure}")
+            sleep(1)
+            clear()
 
-        elif choice == 2:
-            break
-        else:
-            print("[#c01c28]Invalid input try only 1 or 2")
-
+            add_items_to_list(name, amount, generate_id(load_list),measure ,load_list, list_path)
+        
 
 def show_items_menu():
     clear()
