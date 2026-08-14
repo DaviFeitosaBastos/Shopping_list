eb1a964e9db9b6e30a5f71ac4b579b866994ff62
Davi Feitosa <davifeitosabastos@gmail.com>
2026-08-14 16:08:12 -0400

    refactor: removed old and unnecessary checkout

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
 from ui.menus import main_menu
 from utils.validations import get_options
-from rich import print
 from router import router
-from utils.logic import load_shopping_list
 
 
 
@@ -13,20 +11,12 @@ def main():
         main_menu()
         
         choose = get_options()
-
-        if choose not in [0, 1, 2, 3, 4]:
-            continue
             
         # Save the choices
-        action = router.get(choose)
-
-        if action is None:
-            print("[#c01c28]Invalid option, try again.")
-            continue
+        action = router[choose]
 
         _ = action()
 
 
 if __name__ == "__main__":
-    load_list = load_shopping_list()
     main()
