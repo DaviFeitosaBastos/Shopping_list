097adb51a487fd3675274811aab7169d7d0bab9d
Davi Feitosa <davifeitosabastos@gmail.com>
2026-08-14 16:11:56 -0400

    refactor: now it calls the option 3 (remove_items_menu)

--- router.py
+++ router.py
@@ -1,4 +1,4 @@
-from ui.menus import add_items_menu, exit_program, show_items_menu
+from ui.menus import add_items_menu, exit_program, show_items_menu, remove_items_menu
 
 
 
@@ -7,5 +7,6 @@ from ui.menus import add_items_menu, exit_program, show_items_menu
 router = {
     1: add_items_menu,
     2: show_items_menu,
+    3: remove_items_menu,
     0: exit_program
 }
