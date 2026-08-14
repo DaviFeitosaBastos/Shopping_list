f2134064df121b9d14e47b4bf8491a5afc3b246d
Davi Feitosa <davifeitosabastos@gmail.com>
2026-08-14 17:05:19 -0400

    refactor: added search function on it

--- router.py
+++ router.py
@@ -1,4 +1,4 @@
-from ui.menus import add_items_menu, exit_program, show_items_menu, remove_items_menu
+from ui.menus import add_items_menu, exit_program, show_items_menu, remove_items_menu, search_items_menu
 from utils.logic import load_shopping_list
 
 def get_router():
@@ -7,5 +7,6 @@ def get_router():
         1: lambda: add_items_menu(shopping_list),
         2: lambda: show_items_menu(shopping_list),
         3: lambda: remove_items_menu(shopping_list),
+        4: lambda: search_items_menu(shopping_list),
         0: exit_program
     }
