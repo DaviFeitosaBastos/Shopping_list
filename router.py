3d0d53aaa14613449af7b2402eb656ac19968fbb
Davi Feitosa <davifeitosabastos@gmail.com>
2026-08-28 10:10:53 -0400

    refactor: Changed the structure but keeping the same logic

--- router.py
+++ router.py
@@ -1,12 +1,10 @@
 from ui.menus import add_items_menu, exit_program, show_items_menu, remove_items_menu, search_items_menu
-from utils.logic import load_shopping_list
 
 def get_router():
-    shopping_list = load_shopping_list()
     return {
-        1: lambda: add_items_menu(shopping_list),
-        2: lambda: show_items_menu(shopping_list),
-        3: lambda: remove_items_menu(shopping_list),
-        4: lambda: search_items_menu(shopping_list),
+        1: lambda: add_items_menu(),
+        2: lambda: show_items_menu(),
+        3: lambda: remove_items_menu(),
+        4: lambda: search_items_menu(),
         0: exit_program
     }
