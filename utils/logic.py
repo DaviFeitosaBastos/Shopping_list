c71d766547c71dd4b484a6a7ef682ba1074041f0
Davi Feitosa <davifeitosabastos@gmail.com>
2026-08-28 10:10:11 -0400

    refactor: Changed structured to OOP, now the files handling is an object

--- utils/logic.py
+++ utils/logic.py
@@ -1,3 +1,4 @@
+from json.decoder import JSONDecodeError
 from log.logging_setup import get_logger
 from typing import cast, TypedDict
 import os
@@ -13,53 +14,58 @@ class ShoppingItem(TypedDict):
     Item: str
     Amount: float
     Measures: str
+
+
+class FileHandler:
+    absolute_path = os.path.dirname(os.path.abspath(__file__))
     
-
-def load_shopping_list(relative_path: str=list_path) -> list[ShoppingItem]:
-    list_name = os.path.join(utils_dir, relative_path)
+    def __init__(self, filename) -> None:
+        self.file = os.path.join(self.absolute_path, f"database/{filename}")
     
-    try:
-        with open(list_name, "r", encoding="utf-8") as f:
-            return cast(list[ShoppingItem], json.load(f))
-            
-    except FileNotFoundError:
-        with open(list_name, "w", encoding="utf-8") as f:
-            json.dump([], f, indent=4)
-            return cast(list[ShoppingItem], [])
+    def load_files(self):
+        try:
+            with open(self.file, "r", encoding="utf-8") as f:
+                return cast(list[ShoppingItem], json.load(f))
+                
+        except (FileNotFoundError, JSONDecodeError):
+            with open(self.file, "w", encoding="utf-8") as f:
+                json.dump([], f, indent=4)
+                return cast(list[ShoppingItem], [])
 
- 
-def generate_id(shopping_list: list[ShoppingItem]):
-    return len(shopping_list) + 1
+    def generate_id(self):
+        return len(self.load_files()) + 1
 
-
-def add_items_to_list(
-    item_name: str, 
-    quantity: float, 
-    id_: int, measure: str, 
-    shopping_list: list[ShoppingItem],
-    path: str=list_path, 
-):
-    list_name = os.path.join(utils_dir, path)
-    item: ShoppingItem = {
-        "Id": id_,
-        "Item": item_name,
-        "Amount": quantity,
-        "Measures": measure
-    }
-    shopping_list.append(item)
-    
-    with open(list_name, 'w', encoding="utf-8") as f:
-        json.dump(shopping_list, f, indent=4)
-    
-
-
-def remove_items(id: int, shopping_list: list[ShoppingItem], path: str=list_path,) -> None:
-    list_name = os.path.join(utils_dir, path)
-    for index, item in enumerate(shopping_list):
-        if item["Id"] == id:
-            del shopping_list[index]
-            break
-    with open(list_name, 'w', encoding='utf-8') as f:
-        json.dump(shopping_list, f, indent=4)
+    def add_item_to_list(
+        self,
+        item_name: str,
+        quantity: float,
+        measure: str
+    ):
+        shopping_list = self.load_files()
         
+        item: ShoppingItem = {
+            "Id": self.generate_id(),
+            "Item": item_name,
+            "Amount": quantity,
+            "Measures": measure
+        }
+
+        shopping_list.append(item)
+
+        with open(self.file, 'w', encoding='utf8') as f:
+            json.dump(shopping_list, f, indent=2)
+
+    def remove_items(self, id):
+        shopping_list = self.load_files()
+
+        for index, item in enumerate(shopping_list):
+            if item["Id"] == id:
+                del shopping_list[index]
+                break
+                
+        with open(self.file, 'w', encoding='utf-8') as f:
+            json.dump(shopping_list, f, indent=4)
+    
+
+
 
